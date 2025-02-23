c39cbc9c22cf6320e76de38543931239478ee34f
Jakub Chyla <jakchyla@gmail.com>
2025-02-23 22:20:54 +0100

    removed longhorn, removed leftover testing scripts

--- kubernetes/discord_calendar/enter_nginx.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env sh
-
-kubectl -n discord-calendar exec -it deployments/discord-calendar-deployment -c discord-calendar-nginx -- sh
--- kubernetes/discord_calendar/restart.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env sh
-
-kubectl -n discord-calendar delete deployments.apps discord-calendar-deployment && \
-kubectl apply -f deployment.yaml
