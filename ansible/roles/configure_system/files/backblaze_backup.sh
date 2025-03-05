707bebeaad6b92022d37cbd3fc108efa4f9e17aa
Jakub Chyla <jakchyla@gmail.com>
2025-03-05 01:37:03 +0100

    added encryption to backup script

--- ansible/roles/configure_system/files/backblaze_backup.sh
+++ ansible/roles/configure_system/files/backblaze_backup.sh
@@ -1,44 +1,73 @@
 #! /usr/bin/env sh
 
-BACKUP_DIR="/data/chroot_sshfs/nas"
-BACKUP_BUCKET=":b2:backup-hades-storage"
-BACKUP_BUCKET_OLD=":b2:backup-hades-storage-old"
-RCLONE_B2_ACCOUNT="$(systemd-creds cat b2_application_key_id)"
-RCLONE_B2_KEY="$(systemd-creds cat b2_application_key)"
+BACKUP_BUCKET="backup-hades-storage"
+SRC="/data/chroot_sshfs/nas"
+DEST="b2:${BACKUP_BUCKET}/current"
+RCLONE_CONFIG_B2_TYPE="b2"
+RCLONE_CONFIG_B2_ACCOUNT="$(systemd-creds cat b2_application_key_id)"
+RCLONE_CONFIG_B2_KEY="$(systemd-creds cat b2_application_key)"
+RCLONE_CONFIG_CRYPT_TYPE="crypt"
+RCLONE_CONFIG_CRYPT_REMOTE="$DEST"
+RCLONE_CONFIG_CRYPT_PASSWORD="$(systemd-creds cat b2_encryption_password | rclone obscure -)"
 
-# check if $BACKUP_DIR exists
-if ! [ -d "$BACKUP_DIR" ]; then
+# check if $SRC exists
+if ! [ -d "$SRC" ]; then
     printf "%s\n" \
-           "$BACKUP_DIR does not exist" \
+           "\$SRC does not exist" \
            "exiting..."
     exit 1
 fi
 
-# check if $BACKUP_DIR is empty
-if [ -z "$(ls -A "$BACKUP_DIR")" ]; then
+# check if $SRC is empty
+if [ -z "$(ls -A "$SRC")" ]; then
     printf "%s\n" \
-           "$BACKUP_DIR is empty" \
+           "\$SRC is empty" \
            "exiting..."
     exit 1
 fi
 
-# check if $BACKUP_DIR is a mountpoint
-if ! mountpoint "$BACKUP_DIR" > /dev/null; then
+# check if $SRC is a mountpoint
+if ! mountpoint "$SRC" > /dev/null; then
     printf "%s\n" \
-           "$BACKUP_DIR is not a mountpoint" \
+           "\$SRC is not a mountpoint" \
+           "exiting..."
+    exit 1
+fi
+
+if [ -z "$RCLONE_CONFIG_B2_ACCOUNT" ]; then
+    printf "%s\n" \
+           "\$RCLONE_B2_ACCOUNT is empty" \
+           "exiting..."
+    exit 1
+fi
+if [ -z "$RCLONE_CONFIG_B2_KEY" ]; then
+    printf "%s\n" \
+           "\$RCLONE_B2_KEY is empty" \
+           "exiting..."
+    exit 1
+fi
+if [ -z "$RCLONE_CONFIG_CRYPT_PASSWORD" ]; then
+    printf "%s\n" \
+           "\$RCLONE_CRYPT_PASSWORD is empty" \
            "exiting..."
     exit 1
 fi
 
 
-export RCLONE_B2_ACCOUNT
-export RCLONE_B2_KEY
-rclone sync "$BACKUP_DIR" $BACKUP_BUCKET \
-            --backup-dir "$BACKUP_BUCKET_OLD/$(date --iso-8601)" \
+export RCLONE_CONFIG_B2_TYPE
+export RCLONE_CONFIG_B2_ACCOUNT
+export RCLONE_CONFIG_B2_KEY
+export RCLONE_CONFIG_CRYPT_TYPE
+export RCLONE_CONFIG_CRYPT_REMOTE
+export RCLONE_CONFIG_CRYPT_PASSWORD
+rclone sync "$SRC" "crypt:" \
             --fast-list \
             --transfers 16 \
-            --exclude ".123/**" \
             -v
-unset -v RCLONE_B2_ACCOUNT
-unset -v RCLONE_B2_KEY
+unset -v RCLONE_CONFIG_B2_TYPE
+unset -v RCLONE_CONFIG_B2_ACCOUNT
+unset -v RCLONE_CONFIG_B2_KEY
+unset -v RCLONE_CONFIG_CRYPT_TYPE
+unset -v RCLONE_CONFIG_CRYPT_REMOTE
+unset -v RCLONE_CONFIG_CRYPT_PASSWORD
 printf "%s\n" "finished backup"
